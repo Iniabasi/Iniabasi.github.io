94c0f57a51f3e8ab8031e266cc4cd9dc6794939d
kyle Kim <kkim100@uottawa.ca>
2021-03-10 03:51:45 -0500

    added scripts and new icons

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("input").focus(function(){
+		$(this).css("background-color","#F0F8FF");
+	});
+	$("input").blur(function(){
+		$(this).css("background-color","#FFFFFF");
+	});
+
+});
