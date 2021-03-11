c68fdf97a109a74835d6729f791759a1a18d9f17
kyle Kim <kkim100@uottawa.ca>
2021-03-10 21:53:21 -0500

    added date restriction

--- script/script.js
+++ script/script.js
@@ -5,5 +5,34 @@ $(document).ready(function(){
 	$("input").blur(function(){
 		$(this).css("background-color","#FFFFFF");
 	});
+	$('#datepicker').datepicker({
+
+
+	    daysOfWeekDisabled:"6,0",
+    	    startDate: '0d',
+	    autoclose:true
+
+        });
+	
+	$("#btnAnna").click(function(){
+	
+	$('#datepicker').datepicker("setDaysOfWeekDisabled",[0,6,2,5]);
+
+	});
+
+	$("#btnRory").click(function(){
+	$('#datepicker').datepicker("setDaysOfWeekDisabled",[0,6,1,4]);
+
+	});
+	$("#btnGeorge").click(function(){
+
+	$('#datepicker').datepicker("setDaysOfWeekDisabled",[0,6,3,4]);
+	});
+	$("#btnSusan").click(function(){
+
+	$('#datepicker').datepicker("setDaysOfWeekDisabled",[0,6,5]);
+	});
 
 });
+
+
